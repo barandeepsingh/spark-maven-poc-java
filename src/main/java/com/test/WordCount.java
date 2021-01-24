e9c70b6e686c142525c15407ddf0c16eb4dbf885
Barandeep Singh <barandeepsingh@gmail.com>
2021-01-24 17:41:00 +0530

    Changed pom file and wc program

--- src/main/java/com/test/WordCount.java
+++ src/main/java/com/test/WordCount.java
@@ -1,29 +1,35 @@
 package com.test;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
-
-import java.util.Arrays;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 public class WordCount {
 
     private static void wordCount(String fileName) {
 
-        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JD Word Counter");
+//        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JD Word Counter");
 
-        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        SparkSession spark = SparkSession.builder()
+                .appName("spark-demo")
+                .config("spark.master", "local")
+                .getOrCreate();
 
-        JavaRDD<String> inputFile = sparkContext.textFile(fileName);
+//        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+//        JavaRDD<String> inputFile = sparkContext.textFile(fileName);
+
+        Dataset<Row> dataset = spark.read().text(fileName);
+
+        dataset.show(false);
+
 
 //        JavaRDD<String> wordsFromFile = inputFile.flatMap(row -> Arrays.asList(row.split(" ")));
 
-        JavaPairRDD countData = inputFile.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) -> (int) x + (int) y);
+//        JavaPairRDD countData = inputFile.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) -> (int) x + (int) y);
 
-        System.out.println("Processing completed");
-        countData.saveAsTextFile("CountData");
+//        System.out.println("Processing completed");
+//        countData.saveAsTextFile("CountData");
     }
 
     public static void main(String[] args) {
